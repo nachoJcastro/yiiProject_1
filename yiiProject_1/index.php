e77b6dec9bb15bd8a59d2a7bfe2874025e996a0b
nachoJcastro <nachoj.castro@gmail.com>
2014-05-25 17:12:18 -0300

    primer prueba

---
+++ yiiProject_1/index.php
@@ -0,0 +1,18 @@
+<html>
+ <head>
+  <title>Prueba de PHP</title>
+ </head>
+ <body>
+ <?php echo '<p>Hola Mundo</p>'; ?>
+ <?php echo '<br>' ?>
+ <?php echo '<p>Hola Mundo</p>'; ?>
+
+
+<form action="accion.php" method="post">
+ <p>Su nombre: <input type="text" name="nombre" /></p>
+ <p>Su edad: <input type="text" name="edad" /></p>
+ <p><input type="submit" /></p>
+</form>
+
+ </body>
+</html>
